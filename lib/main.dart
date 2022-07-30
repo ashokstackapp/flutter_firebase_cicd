8c40d410eca7d771e15f73efaa76b48642680ea3
Navnit <navneetnavadiya@gmail.com>
2022-07-30 12:12:05 +0530

    Update workflow

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo CI/CD',
+      title: 'Flutter Demo CI/CD2',
       theme: ThemeData(
         // This is the theme of your application.
         //
